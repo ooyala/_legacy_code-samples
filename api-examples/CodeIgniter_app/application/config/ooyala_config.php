a0814a89fa12b46247c2282201b97d3e8197e70c
Felipe Madrigal <luis.felipe@ooyala.com>
2013-10-30 14:20:57 -0600

    add google ima tag field

--- api-examples/CodeIgniter_app/application/config/ooyala_config.php
+++ api-examples/CodeIgniter_app/application/config/ooyala_config.php
@@ -1,9 +1,10 @@
 <?php
-$config['api_key'] = "YOUR KEY";
-$config['api_secret'] = "YOUR SECRET";
-$config['p_code'] = "YOUR P_CODE";
-$config['default_embed_code'] = "EMBED_CODE";
-$config['default_player_id'] = "PLAYER_ID";
-$config['google_ima_player_id'] = "IMA_PLAYER_ID";
+$config['api_key'] = "YOUR_API_KEY";
+$config['api_secret'] = "YOUR_API_SECRET";
+$config['p_code'] = "YOUR_P_CODE";
+$config['default_embed_code'] = "YOUR_DEFAULT_EMBED_CODE";
+$config['default_player_id'] = "YOUR_DEFAULT_PLAYER_ID";
+$config['google_ima_player_id'] = "YOUR_GOOGLE_IMA_PLAYER_ID";
 $config['sample_email'] = "alice@ooyala.com";
+$config['google_ima_ad_tag'] = "YOUR_GOOGLE_IMA_AD_TAG";
 ?>
