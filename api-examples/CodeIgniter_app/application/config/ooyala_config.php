bd211f84ca5d7f71251a51dcde217c0ff923cab8
Felipe Madrigal <luis.felipe@ooyala.com>
2013-10-30 12:12:16 -0600

    Add missing config file

---
+++ api-examples/CodeIgniter_app/application/config/ooyala_config.php
@@ -0,0 +1,9 @@
+<?php
+$config['api_key'] = "YOUR KEY";
+$config['api_secret'] = "YOUR SECRET";
+$config['p_code'] = "YOUR P_CODE";
+$config['default_embed_code'] = "EMBED_CODE";
+$config['default_player_id'] = "PLAYER_ID";
+$config['google_ima_player_id'] = "IMA_PLAYER_ID";
+$config['sample_email'] = "alice@ooyala.com";
+?>
