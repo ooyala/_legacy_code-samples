5d5b5a11dbb16236c9622a3a1482b34470e48185
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-19 14:30:07 -0600

    Add comments to the config file

--- api-examples/CodeIgniter_app/application/config/ooyala_config.php
+++ api-examples/CodeIgniter_app/application/config/ooyala_config.php
@@ -1,10 +1,22 @@
 <?php
+// You can find your api_key and secret in Backlot. See
+// http://support.ooyala.com/developers/documentation/concepts/api_keys.html
 $config['api_key'] = "YOUR_API_KEY";
 $config['api_secret'] = "YOUR_API_SECRET";
 $config['p_code'] = "YOUR_P_CODE";
+
+// Default embed code and player id to use on the examples.
+// You can grab anyone from your Backlot account
 $config['default_embed_code'] = "YOUR_DEFAULT_EMBED_CODE";
 $config['default_player_id'] = "YOUR_DEFAULT_PLAYER_ID";
+
+// Since Google IMA is set on a player basis, you need to specify
+// a player that has Google IMA enabled in order for the examples to work
 $config['google_ima_player_id'] = "YOUR_GOOGLE_IMA_PLAYER_ID";
-$config['sample_email'] = "alice@ooyala.com";
 $config['google_ima_ad_tag'] = "YOUR_GOOGLE_IMA_AD_TAG";
+
+// These are optional parameters. You can choose to leave the defaults
+// or change them to your convinience
+$config['sample_email'] = "alice@ooyala.com";
+
 ?>
