5e5bb5a19c27ea2aa6b2dd8d20d046ad27c75cbe
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-19 12:29:08 -0600

    Hide related-video div if the screen isn't big enough

--- api-examples/CodeIgniter_app/application/assets/javascript/web-sample-mobile.js
+++ api-examples/CodeIgniter_app/application/assets/javascript/web-sample-mobile.js
@@ -4,6 +4,22 @@ $ = OO.$;
 _ = OO._;
 console.log("related videos", related_videos);
 
+$(document).ready(function() {
+    var availableWidth = $(window).width();
+    var availableHeight = $(window).height();
+    console.log("width", availableWidth);
+    if (availableWidth < 700) {
+        $(".related_video_container").hide();
+        $("#playerwrapper").width("100%");
+        // Use aspect ratio 4:3
+        var computedWidth = $("#playerwrapper").width();
+        var playerHeight = computedWidth * (3/4);
+        $("#playerwrapper").height(playerHeight);
+    } else {
+        $(".related_video_container").show();
+    }
+});
+
 this.mb.subscribe(OO.EVENTS.CONTENT_TREE_FETCHED, "func1", function (eventName, arg1, arg2) {
     var video_title = arg1.title;
     var video_description = arg1.description;
@@ -22,10 +38,13 @@ this.mb.subscribe(OO.EVENTS.METADATA_FETCHED, "func2", function (eventName, arg1
 
 // Assign related and trending videos
 var releatedVideoContainer = $('#related_video_container');
+var trendingVideoContainer = $('.related_video_container');
 for (var i = 0; i < related_videos.length; i++){
     try{
         var queryRelated = "#related_" + i;
+        var queryTrending = "#trending_" + i;
         releatedVideoContainer.find(queryRelated).attr("src", related_videos[i].preview_image_url);
+        trendingVideoContainer.find(queryTrending).attr("src", trending_videos[i].preview_image_url);
     } catch(e) {
         // do nothing
     }
@@ -36,4 +55,11 @@ $(".related_video").click( function(event){
     var id = callingContainer.charAt(callingContainer.length - 1);
     var embedCode = related_videos[id].embed_code;
     videoPlayer.setCurrentItemEmbedCode(embedCode);
+});
+
+$(".trending_video").click( function(event){
+     var callingContainer = event.target.id;
+     var id = callingContainer.charAt(callingContainer.length - 1);
+     var embedCode = trending_videos[id].embed_code;
+     videoPlayer.setCurrentItemEmbedCode(embedCode);
 });
