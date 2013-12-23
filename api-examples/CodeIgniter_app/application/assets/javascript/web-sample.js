0822a465659f4214111ff7d32d7ccda486dde658
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-23 14:36:26 -0600

    Change JS style from plain to OO.plugin

--- api-examples/CodeIgniter_app/application/assets/javascript/web-sample.js
+++ api-examples/CodeIgniter_app/application/assets/javascript/web-sample.js
@@ -1,50 +1,85 @@
-this.mb = window.mb;
+// Define a plugin to interact with the player.
+// It helps us create a namepsace
+// and hold references to helper libraries
+OO.plugin("WebSample", function (OO, _, $, W) {
+  WebExample = function (mb, id) {
+      this.mb = mb;
+      this.id = id;
+      console.log("related videos", related_videos);
+      console.log("trending videos", trending_videos);
+      this.init();
+  };
 
-$ = OO.$;
-_ = OO._;
-console.log("related videos", related_videos);
-console.log("trending videos", trending_videos);
+  _.extend(WebExample.prototype, {
+    init: function(){
+      // subscribe to relevant events
+      this.mb.subscribe(OO.EVENTS.CONTENT_TREE_FETCHED,
+        'WebSample', _.bind(this.onContentFetched, this));
+      this.mb.subscribe(OO.EVENTS.METADATA_FETCHED,
+        'WebSample', _.bind(this.onMetadataFetched, this));
+      this.mb.subscribe(OO.EVENTS.PLAYER_CREATED,
+        'WebSample', _.bind(this.onPlayerCreated, this));
+    },
 
-this.mb.subscribe(OO.EVENTS.CONTENT_TREE_FETCHED, "func1", function (eventName, arg1, arg2) {
-    var video_title = arg1.title;
-    var video_description = arg1.description;
+    onContentFetched: function (eventName, content) {
+      var video_title = content.title;
+      var video_description = content.description;
+      $("#video_title").text(video_title);
+      $("#description").text(video_description);
+    },
 
-    $("#video_title").text(video_title);
-    $("#description").text(video_description);
-});
+    onMetadataFetched: function (eventName, metadata) {
+      var metadata_as_string = JSON.stringify(metadata.base);
+      console.log(metadata);
+      console.warn(JSON.stringify(metadata));
+      $("#metadata").text(metadata_as_string);
+    },
 
-this.mb.subscribe(OO.EVENTS.METADATA_FETCHED, "func2", function (eventName, arg1, arg2){
-    var metadata = arg1.base;
-    var metadata_as_string = JSON.stringify(metadata);
-    console.log(metadata);
-    console.log(JSON.stringify(metadata));
-    $("#metadata").text(metadata_as_string);
-});
-
-// Assign related and trending videos
-var releatedVideoContainer = $('#related_video_container');
-var trendingVideoContainer = $('.related_video_container');
-for (var i = 0; i < related_videos.length; i++){
-    try{
-        var queryRelated = "#related_" + i;
-        var queryTrending = "#trending_" + i;
-        releatedVideoContainer.find(queryRelated).attr("src", related_videos[i].preview_image_url);
-        trendingVideoContainer.find(queryTrending).attr("src", trending_videos[i].preview_image_url);
-    } catch(e) {
-        // do nothing
+    onPlayerCreated: function (eventName, elementId) {
+      window.playerRoot = $("#" + elementId);
     }
-}
 
-$(".related_video").click( function(event){
-    var callingContainer = event.target.id;
-    var id = callingContainer.charAt(callingContainer.length - 1);
-    var embedCode = related_videos[id].embed_code;
-    videoPlayer.setCurrentItemEmbedCode(embedCode);
+  });
+  return WebExample;
 });
 
-$(".trending_video").click( function(event){
-     var callingContainer = event.target.id;
-     var id = callingContainer.charAt(callingContainer.length - 1);
-     var embedCode = trending_videos[id].embed_code;
-     videoPlayer.setCurrentItemEmbedCode(embedCode);
+// Non-player related
+
+// Wait until the player is ready for playback
+OO.ready(function (OO){
+  // If jQuery is not defined, use the jQuery version from the player
+  if (!$) {
+    $ = OO.$;
+  }
+
+  // Reference to the related and trending video containers to avoid
+  // having to search the entire DOM when looking for specific elements
+  var releatedVideoContainer = $('#related_video_container');
+  var trendingVideoContainer = $('.related_video_container');
+  for (var i = 0; i < related_videos.length; i++) {
+      try {
+          // Set the image src to one of the related/trending videos
+          var queryRelated = "#related_" + i;
+          var queryTrending = "#trending_" + i;
+          releatedVideoContainer.find(queryRelated).attr("src", related_videos[i].preview_image_url);
+          trendingVideoContainer.find(queryTrending).attr("src", trending_videos[i].preview_image_url);
+      } catch(e) {
+          // do nothing
+      }
+  }
+
+  // When user clicks a related or trending video change the current video.
+  $(".related_video").click( function(event) {
+      var callingContainer = event.target.id;
+      var id = callingContainer.charAt(callingContainer.length - 1);
+      var embedCode = related_videos[id].embed_code;
+      videoPlayer.setCurrentItemEmbedCode(embedCode);
+  });
+
+  $(".trending_video").click( function(event) {
+       var callingContainer = event.target.id;
+       var id = callingContainer.charAt(callingContainer.length - 1);
+       var embedCode = trending_videos[id].embed_code;
+       videoPlayer.setCurrentItemEmbedCode(embedCode);
+  });
 });
