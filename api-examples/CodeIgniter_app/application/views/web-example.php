b1e1fa8907c7b3cc053b62232979624c497aff4e
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-23 14:50:38 -0600

    Player creation is now inside and OO.ready call

--- api-examples/CodeIgniter_app/application/views/web-example.php
+++ api-examples/CodeIgniter_app/application/views/web-example.php
@@ -3,13 +3,19 @@
  <head>
    <script src="http://player.ooyala.com/v3/<?php echo $player_id; ?>"></script>
    <script src="<?php echo base_url();?>/application/assets/javascript/gaTrack.js"></script>
+   <script>
+      ooyalaGaTrackSettings = {
+        verboseLogging: true
+      }
+    </script>
    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/application/assets/css/web-sample.css">
   </head>
+
   <body>
     <h1> Web example </h1>
     <h1 id="video_title"> Title </h1>
       <div class="main">
-         <div id='playerwrapper'></div>
+         <div id='playerwrapper' class="player"></div>
          <p id="description">
             Description
           </p>
@@ -33,14 +39,18 @@
           </div>
       </div>
      <script>
-         var videoPlayer =
-            OO.Player.create('playerwrapper', '<?php echo $embed_code; ?>', {
+        OO.ready(function (){
+           window.videoPlayer =
+              OO.Player.create('playerwrapper', '<?php echo $embed_code; ?>', {
+
+          });
         });
+
       </script>
       <script>
         var related_videos = <?php echo $related_videos;?>;
         var trending_videos = <?php echo $trending_videos;?>;
-        var mb = videoPlayer.mb
+
       </script>
 
 
@@ -57,13 +67,14 @@
 
       <script src="<?php echo base_url();?>/application/assets/javascript/web-sample.js"> </script>
       <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-304811-17', 'localdevelopment.com');
-        ga('send', 'pageview');
+       ga('create', 'UA-304811-17', 'localdevelopment.com');
+       ga('send', 'pageview');
       </script>
+
   </body>
 </html>
