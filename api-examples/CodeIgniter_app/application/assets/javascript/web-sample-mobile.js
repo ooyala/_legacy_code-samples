6444dc41ab66fb69bfc72f8bb05c055ab9e73656
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-26 14:48:46 -0600

    Encapsulate resizing logic in a OO.ready call

--- api-examples/CodeIgniter_app/application/assets/javascript/web-sample-mobile.js
+++ api-examples/CodeIgniter_app/application/assets/javascript/web-sample-mobile.js
@@ -3,18 +3,23 @@
 // on document.ready
 // This is meant as an example of what you can achieve
 // with mobile versions of your website
-
-$(document).ready(function() {
-    var windowWidth = $(window).width();
-    if (windowWidth < 700) {
-        $(".related_video_container").hide();
-        // Use the full width of the player to display the player
-        $("#playerwrapper").width("100%");
-        // Use aspect ratio 4:3
-        var computedWidth = $("#playerwrapper").width();
-        var playerHeight = computedWidth * (3/4);
-        $("#playerwrapper").height(playerHeight);
-    } else {
-        $(".related_video_container").show();
-    }
+OO.ready(function (OO) {
+  if (!$) {
+    $ = OO.$;
+  }
+  $(document).ready(function() {
+      var windowWidth = $(window).width();
+      if (windowWidth < 700) {
+          console.log(windowWidth);
+          $(".related_video_container").hide();
+          // Use the full width of the player to display the player
+          $("#playerwrapper").width("100%");
+          // Use aspect ratio 4:3
+          var computedWidth = $("#playerwrapper").width();
+          var playerHeight = computedWidth * (3/4);
+          $("#playerwrapper").height(playerHeight);
+      } else {
+          $(".related_video_container").show();
+      }
+  });
 });
