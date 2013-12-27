5dc4f00def61b024d6775132d3ef6340560f2e55
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-26 19:55:18 -0600

    Surround player creation in OO.ready call

--- application/views/message-bus-advanced.php
+++ application/views/message-bus-advanced.php
@@ -4,10 +4,12 @@
     <script src="http://player.ooyala.com/v3/<?php echo $player_id; ?>"></script>
     <div id='playerContainer'></div>
     <script>
+OO.ready( function(OO) {
+    var videoPlayer = OO.Player.create('playerContainer', '<?php echo $embed_code; ?>', { width: 640, height: 360 });
+    // Preserve the mb to acces it within simple_milestones
+    var mb = videoPlayer.mb
+});
 
-      var videoPlayer = OO.Player.create('playerContainer', '<?php echo $embed_code; ?>', { width: 640, height: 360 });
-      // Preserve the mb to acces it within simple_milestones
-      var mb = videoPlayer.mb
     </script>
       <div id="logName"><textarea id="textLog" style="width:640px; height:300px;">Log file:</textarea></div>
 
