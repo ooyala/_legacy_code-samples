517ef49c9e563e1838edc554661564f64a129065
Felipe Madrigal <luis.felipe@ooyala.com>
2013-12-26 18:24:31 -0600

    Change syntax to OO.plugin

--- application/assets/javascript/milestones_with_ads.js
+++ application/assets/javascript/milestones_with_ads.js
@@ -1,111 +1,142 @@
-this.mb = window.mb;
+OO.plugin("MilestoneWithAds", function (OO, _, $, W) {
+  MilestoneWithAds = function(mb, id) {
+    this.mb = mb;
+    this.id = id;
+    var playerIsFlash = false;
 
-var playerIsFlash = false;
+    // This are the milestones to track. The way we are tracking them
+    // is setting a boolean each time the video passes a milestone. For
+    // a different approach, check gaTrack.js
+    var videoStarted = false;
+    var _25per = false;
+    var _50per = false;
+    var _75per = false;
+    var videoEnded = false;
 
-// This are the milestones to track
-var videoStarted = false;
-var _25per = false;
-var _50per = false;
-var _75per = false;
-var videoEnded = false;
+    var videoLength = 0;
+    var _25milestone;
+    var _50milestone;
+    var _75milestone;
 
-var videoLength = 0;
-var _25milestone;
-var _50milestone;
-var _75milestone;
+    var isAdPlaying = false;
+    this.init();
+  };
 
-var isAdPlaying = false;
+  _.extend(MilestoneWithAds.prototype, {
+    init: function() {
+      // Subscribe to relevant events
+      this.mb.subscribe(OO.EVENTS.PLAYBACK_READY,
+        "MilestoneWithAds", _.bind(this.onPlaybackReady, this));
+      this.mb.subscribe(OO.EVENTS.WILL_PLAY_ADS,
+        "MilestoneWithAds", _.bind(this.onWillPlayAds, this));
+      this.mb.subscribe(OO.EVENTS.ADS_PLAYED,
+        "MilestoneWithAds", _.bind(this.onAdsPlayed, this));
+      this.mb.subscribe(OO.EVENTS.CONTENT_TREE_FETCHED,
+        "MilestoneWithAds", _.bind(this.onContentTreeFetched, this));
+      this.mb.subscribe(OO.EVENTS.PLAYING,
+        "MilestoneWithAds", _.bind(this.onPlaying, this));
+      this.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED,
+        "MilestoneWithAds", _.bind(this.onPlayheadTimeChanged, this));
+      this.mb.subscribe(OO.EVENTS.PLAYED,
+        "MilestoneWithAds", _.bind(this.onPlayed, this));
+      this.write("Log here");
+    },
 
-this.mb.subscribe(OO.EVENTS.PLAYBACK_READY, "func0", function (eventName){
-  playerIsFlash = isFlash();
+    // Helper functions
+    isFlash: function() {
+      try {
+        return OO.requiredInEnvironment('flash-playback');
+      }
+      catch(e) {
+        // Fallback, asume is Flash
+        return true;
+      }
+    },
+
+    write: function(text) {
+        var textLog = document.getElementById("textLog");
+        textLog.innerHTML = textLog.value+new Date()+":"+text+"\n";
+    },
+
+    // Event functions
+    onPlaybackReady: function() {
+      playerIsFlash = isFlash();
+    },
+
+    onWillPlayAds: function() {
+      this.write("An ad will be played");
+      isAdPlaying = true;
+    },
+
+    onAdsPlayed: function() {
+      this.write("Ads finished");
+      isAdPlaying = false;
+    },
+
+    onContentTreeFetched: function (eventName, content) {
+      // Flash reports the duration as "time" in seconds
+      if (playerIsFlash) {
+        videoLength = content.time;
+      } else {
+        // HTML reports video in miliseconds and as "duration"
+        videoLength = content.duration;
+        videoLength = videoLength / 1000;
+      }
+      // Define the duration of the milestones
+      _25milestone = 1 * (videoLength / 4);
+      _50milestone = 2 * (videoLength / 4);
+      _75milestone = 3 * (videoLength / 4);
+    },
+
+    onPlaying: function() {
+      if (isAdPlaying) {
+        // Ignore the event if it's not the main video
+        return;
+      }
+      if (videoEnded) {
+        // Reset all values
+        // We are asuming we hit replay on the same video
+        videoStarted = false;
+        _25per = false;
+        _50per = false;
+        _75per = false;
+        videoEnded = false;
+        this.write("Replaying video");
+      }
+
+      if (!videoStarted){
+        videoStarted = true;
+        this.write("Video began playback");
+      }
+    },
+
+    onPlayheadTimeChanged: function (eventName, currentTime) {
+      // Ignore the event if it's not the main video
+      if (isAdPlaying) {
+        return;
+      }
+
+      // We check from first to last to account for scrubbing
+      if (currentTime > _75milestone && !_75per) {
+        _25per = true;
+        _50per = true;
+        _75per = true;
+        this.write("We hit the 75% milestone");
+      } else if (currentTime > _50milestone && !_50per) {
+        _25per = true;
+        _50per = true;
+        this.write("We hit the 50% milestone");
+      } else if (currentTime > _25milestone && !_25per) {
+        _25per = true;
+        this.write("We hit the 25% milestone");
+      }
+    },
+
+    onPlayed: function() {
+      videoEnded = true;
+      this.write("Video endeded");
+    }
+  });
+
+  return MilestoneWithAds;
 });
-
-this.mb.subscribe(OO.EVENTS.WILL_PLAY_ADS, "A", function(eventName){
-  write("An ad will be played");
-  isAdPlaying = true;
-});
-
-this.mb.subscribe(OO.EVENTS.ADS_PLAYED, "B", function(eventName){
-  write("Ads finished");
-  isAdPlaying = false;
-});
-
-this.mb.subscribe(OO.EVENTS.CONTENT_TREE_FETCHED, "func1", function (eventName, arg1, arg2) {
-  console.log(eventName, arg1, arg2);
-  if (playerIsFlash){
-    videoLength = arg1.time;
-  }
-  else{
-    // HTML reports video in miliseconds
-    videoLength = arg1.duration;
-    videoLength = videoLength / 1000;
-  }
-  _25milestone = 1 * (videoLength / 4);
-  _50milestone = 2 * (videoLength / 4);
-  _75milestone = 3 * (videoLength / 4);
-});
-
-this.mb.subscribe(OO.EVENTS.PLAYING, "func2", function (eventName, arg1, arg2) {
-  if (isAdPlaying){
-    // Ignore the event if it's not the main video
-    return;
-  }
-  if (videoEnded){
-    // Reset all values
-    // We are asuming we hit replay on the same video
-    videoStarted = false;
-    _25per = false;
-    _50per = false;
-    _75per = false;
-    videoEnded = false;
-    write("Replaying video");
-  }
-
-  if (!videoStarted){
-    videoStarted = true;
-    write("Video began playback");
-  }
-});
-
-this.mb.subscribe(OO.EVENTS.PLAYHEAD_TIME_CHANGED, "func3", function (eventName, currentTime) {
-  // We check from first to last to account for scrubbing
-  if (isAdPlaying){
-    // Ignore the event if it's not the main video
-    return;
-  }
-  if (currentTime > _75milestone && !_75per){
-    _25per = true;
-    _50per = true;
-    _75per = true;
-    write("We hit the 75% milestone");
-  }
-  else if (currentTime > _50milestone && !_50per) {
-    _25per = true;
-    _50per = true;
-    write("We hit the 50% milestone");
-  }
-  else if (currentTime > _25milestone && !_25per){
-    _25per = true;
-    write("We hit the 25% milestone");
-  }
-});
-
-this.mb.subscribe(OO.EVENTS.PLAYED, "func4", function (eventName){
-  videoEnded = true;
-  write("Video endeded");
-});
-
-function isFlash(){
-  try{
-    return OO.__internal.requiredInEnvironment('flash-playback');
-  }
-  catch(e){
-    // Fallback, asume true
-    return true;
-  }
-}
-
-function write(text) {
-    var textLog = document.getElementById("textLog");
-    textLog.innerHTML = textLog.value+new Date()+":"+text+"\n";
-}
