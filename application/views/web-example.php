c793b5582edbe8fc30ab8dcc042fd10743525d3e
Fernando Padilla <rpfernando@ooyala.com>
2014-01-09 17:20:27 -0600

    Minor css modifications

--- application/views/web-example.php
+++ application/views/web-example.php
@@ -13,12 +13,12 @@ the controller web_example.php
       verboseLogging: true
     }
   </script>
- <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/application/assets/css/web-sample.css">
+ <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>application/assets/css/web-sample.css">
 </head>
 
 <body>
   <div class="main-wrap">
-    <ul class="nav" data-0="position:absolute;" data-630="position:fixed;">
+    <ul class="nav skrollable skrollable-between" data-0="position:absolute;" data-630="position:fixed;">
       <li><a href="#"> <img class="logo" src="http://www.ooyala.com/careers-mx/2014/assets/images/logo.png"></img> </a></li>
       <li>
         <a href="#"> Web example </a>
@@ -35,8 +35,8 @@ the controller web_example.php
         </ul>
       </li>
       <li><a href="#">Options</a></li>
-      <!-- Those will be shown from right to left in the nav 
-              starting from firstone to lastone -->
+      <!-- These will be shown from right to left in the nav 
+                starting from firstone to lastone -->
       <li class="right"><a href="#">Log In</a></li>
       <li class="right"><a href="#">Try it now</a></li>
     </ul>
@@ -57,40 +57,56 @@ the controller web_example.php
       <div class="related-video-container">
         <h2>Related</h2>
         <div class="video-container">
-          <img id="related-1" class="video related-video" src=""/>
-          <p id="related-caption-1" class="caption">Caption x</p>
+          <a href="#">
+            <img id="related-1" class="video related-video" src=""/>
+            <p id="related-caption-1" class="caption">Caption x</p>
+          </a>
         </div>
         <div class="video-container">
-          <img id="related-2" class="video related-video" src=""/>
-          <p id="related-caption-2" class="caption">Caption x</p>
+          <a href="#">
+            <img id="related-2" class="video related-video" src=""/>
+            <p id="related-caption-2" class="caption">Caption x</p>
+          </a>
         </div>
         <div class="video-container">
-          <img id="related-3" class="video related-video" src=""/>
-          <p id="related-caption-3"class="caption">Caption x</p>
+          <a href="#">
+            <img id="related-3" class="video related-video" src=""/>
+            <p id="related-caption-3"class="caption">Caption x</p>
+          </a>
         </div>
         <div class="video-container">
-          <img id="related-4" class="video related-video" src=""/>
-          <p id="related-caption-4"class="caption">Caption x</p>
+          <a href="#">
+            <img id="related-4" class="video related-video" src=""/>
+            <p id="related-caption-4"class="caption">Caption x</p>
+          </a>
         </div>
       </div>
 
       <div class="trending-video-container">
         <h2>Treanding</h2>
         <div class='video-container'>
-          <img id="trending-1" class="video trending-video" src=""/>
-          <p id="trending-caption-1" class="caption">Trending</p>
+          <a href="#">
+            <img id="trending-1" class="video trending-video" src=""/>
+            <p id="trending-caption-1" class="caption">Trending</p>
+          </a>
         </div>
         <div class='video-container'>
-          <img id="trending-2" class="video trending-video" src=""/>
-          <p id="trending-caption-2" class="caption">Trending</p>
+          <a href="#">
+            <img id="trending-2" class="video trending-video" src=""/>
+            <p id="trending-caption-2" class="caption">Trending</p>
+          </a>
         </div>
         <div class="video-container">
-          <img id="trending-3" class="video related-video" src=""/>
-          <p id="trending-caption-3"class="caption">Caption x</p>
+          <a href="#">
+            <img id="trending-3" class="video related-video" src=""/>
+            <p id="trending-caption-3"class="caption">Caption x</p>
+          </a>
         </div>
         <div class="video-container">
-          <img id="trending-4" class="video related-video" src=""/>
-          <p id="trending-caption-4"class="caption">Caption x</p>
+          <a href="#">
+            <img id="trending-4" class="video related-video" src=""/>
+            <p id="trending-caption-4"class="caption">Caption x</p>
+          </a>
         </div>
       </div>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
@@ -108,7 +124,7 @@ the controller web_example.php
   <script src="<?php echo base_url();?>/application/assets/javascript/web-sample.js"> </script>
 
   <!-- Library used to move the nav through the page -->
-  <script type="text/javascript" src="https://raw.github.com/Prinzhorn/skrollr/master/dist/skrollr.min.js"></script>
+  <script type="text/javascript" src="http://prinzhorn.github.io/skrollr/dist/skrollr.min.js"></script>
   <script type="text/javascript">
     var s = skrollr.init();
   </script>
